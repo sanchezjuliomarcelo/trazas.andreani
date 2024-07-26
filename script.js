c8aee20be98ca801f3cc1f5b929f6b2f64a4fa60
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-07-26 20:47:27 -0300

    1

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 document.querySelector('#numeroAndreaniForm').addEventListener('submit', async (event) => {
     event.preventDefault();
   
-    const user = 'tiendasdigitalessrl_gla';  // Reemplaza con tu usuario real
-    const password = '[e]Xn1Y80iq9';  // Reemplaza con tu contraseña real
+    const user = import.meta.env.VITE_API_USER; 
+    const password = import.meta.env.VITE_API_PASSWORD;
     const credentials = btoa(`${user}:${password}`);
   
-    const numeroAndreani = document.querySelector('.numeroAndreani').value;
-    const tokenUrl = "https://apis.andreani.com/login";
-    const tracingsUrl = `https://apis.andreani.com/v2/envios/${numeroAndreani}/trazas`;
-  
     try {
-      // Obtener el token
-      const tokenResponse = await fetch(tokenUrl, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -19,28 +14,30 @@ document.querySelector('#numeroAndreaniForm').addEventListener('submit', async (
           'Authorization': `Basic ${credentials}`,
         },
       });
-      if (!tokenResponse.ok) throw new Error('Error al obtener el token');
-      const tokenData = await tokenResponse.json();
-      const token = tokenData.token;
+      const data = await response.json();
+      sessionStorage.setItem("x-authorization-token", data.token);
   
-      // Mostrar el loader
-      document.getElementById("loader").style.display = 'none';
-      document.getElementById("cargando").style.display = 'block';
+      const numeroAndreani = document.querySelector('.numeroAndreani').value;
+      
+      const $loader = document.getElementById("loader");
+      $loader.style.display = 'block';
+      const $cargando = document.getElementById("cargando");
+      $cargando.style.display = 'block';
+      const $infonumeroAndreani = document.getElementById("infonumeroAndreani");
+      $infonumeroAndreani.style.display = 'none';
   
-      // Obtener los datos del envío
-      const tracingResponse = await fetch(tracingsUrl, {
+      const enviosResponse = await fetch(`${import.meta.env.VITE_API_URL}/v2/envios/${numeroAndreani}/trazas`, {
         method: 'GET',
         headers: {
-          'x-authorization-token': token,
+          'x-authorization-token': sessionStorage.getItem('x-authorization-token'),
         },
       });
-      if (!tracingResponse.ok) throw new Error('Error al obtener los datos del envío');
-      const tracingData = await tracingResponse.json();
-  
-      // Procesar la respuesta
+      const enviosData = await enviosResponse.json();
       let infonumeroAndreani = "";
-      if (tracingData.eventos.length === 0) {
-        infonumeroAndreani = `
+  
+      if (typeof enviosData.Estado === 'undefined') {
+        exibeIcone("remove");
+        infonumeroAndreani += `
         <div class="alert alert-info alert-dismissible fade show" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -48,31 +45,32 @@ document.querySelector('#numeroAndreaniForm').addEventListener('submit', async (
           Número de envío inexistente.
         </div>`;
       } else {
-        infonumeroAndreani = tracingData.eventos.map(evento => `
+        exibeIcone("check");
+        infonumeroAndreani += `
         <div class="alert alert-secondary alert-dismissible fade show" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h4 class="alert-heading">${evento.Estado}</h4>
+          <h4 class="alert-heading">${enviosData.Estado}</h4>
           <hr>
-          <p class="m-0"><strong>Fecha: </strong>${evento.Fecha}</p>
-          <p class="m-0"><strong>Estado: </strong>${evento.Estado}</p>
-          <p class="m-0"><strong>Traducción: </strong>${evento.Traduccion}</p>
-          <p class="m-0"><strong>Sucursal: </strong>${evento.Sucursal}</p>
-          <p class="m-0"><strong>Ciclo: </strong>${evento.Ciclo}</p>
-          ${evento.Motivo ? `<p class="m-0"><strong>Motivo: </strong>${evento.Motivo}</p>` : ''}
-        </div>
-        `).join('');
+          <p class="m-0"><strong>Fecha: </strong>${enviosData.Fecha}</p>
+          <p class="m-0"><strong>Código de estado: </strong>${enviosData.EstadoId}</p>
+          <p class="m-0"><strong>Traducción: </strong>${enviosData.Traduccion}</p>
+          <p class="m-0"><strong>Sucursal: </strong>${enviosData.Sucursal}</p>
+          <p class="m-0"><strong>Ciclo: </strong>${enviosData.Ciclo}</p>
+        </div>`;
       }
   
       document.querySelector("#infonumeroAndreani").innerHTML = infonumeroAndreani;
-  
     } catch (error) {
       console.error(error);
       alert('Error al realizar la consulta. Por favor, intente de nuevo más tarde.');
     } finally {
-      document.getElementById("loader").style.display = 'none';
-      document.getElementById("cargando").style.display = 'none';
+      const $loader = document.getElementById("loader");
+      $loader.style.display = 'none';
+      const $cargando = document.getElementById("cargando");
+      $cargando.style.display = 'none';
     }
   });
+  
   
