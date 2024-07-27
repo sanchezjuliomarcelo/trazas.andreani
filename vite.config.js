b760ce3fddf9904358de0e901591857e11d81a3f
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-07-26 23:49:35 -0300

    cli

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://apis.andreani.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://apis.andreani.com', // Cambia esto por tu URL de destino
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
+
